66c3ab3dfec014bef13eb9ccfaf2187a785ebca0
Alex Kovalchuk <alexanderkoval203@gmail.com>
2021-03-12 14:21:53 -0800

    bookmarks and notest

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -1,79 +1,94 @@
 <template>
-  <div class="md:mx-56 md:my-12 font-roboto">
-    <h1 class="border-b border-gray-400 md:text-3xl md:pb-4">Account Settings</h1>
-    <div class="md:flex md:mt-16">
-      <div class="md:w-3/12">
-        <img class="md:w-24 md:h-auto" loading="lazy" decoding="async" src="~/assets/images/profile.svg" alt="">
-        <button class="md:ml-6 md:mt-4 font-medium focus:outline-none">Update</button>
+  <div>
+    <Header>
+      <img id="notification" src="~/assets/images/notification-icon.svg" alt="">
+      <img id="profile" src="~/assets/images/profile.svg" alt="">
+      <button class="primary-btn">write</button>
+    </Header>
+    <div class="md:mx-56 md:my-12">
+      <h1 class="border-b border-gray-400 md:text-3xl md:pb-4">Account Settings</h1>
+      <div class="md:flex md:mt-16">
+        <div class="md:w-3/12">
+          <img class="md:w-24 md:h-auto" loading="lazy" decoding="async" src="~/assets/images/profile.svg" alt="">
+          <button class="md:ml-6 md:mt-4 font-medium focus:outline-none">Update</button>
+        </div>
+        <div class="md:w-9/12">
+          <form action="">
+            <div class="md:mb-6">
+              <label class="text-gray-600 md:mb-2 md:inline-block">Name</label>
+              <div class="md:flex md:items-center">
+                <input v-if="allowUpdate.name" type="text" v-model="user.name"
+                       class="border border-gray-400 rounded outline-none md:py-1 md:px-2"
+                       name="name">
+                <div v-else>Sowmay</div>
+                <button type="button" @click="allowUpdateHandler('name')"
+                        class="md:ml-6 font-medium outline-none focus:outline-none">Update
+                </button>
+              </div>
+            </div>
+            <div class="md:mb-6">
+              <label class="text-gray-600 md:mb-2 md:inline-block">Username</label>
+              <div class="md:flex md:items-center">
+                <input v-if="allowUpdate.username" type="text" v-model="user.username"
+                       class="border border-gray-400 rounded outline-none md:py-1 md:px-2"
+                       name="name">
+                <div v-else>@champ.sowmay</div>
+                <button type="button" @click="allowUpdateHandler('username')"
+                        class="md:ml-6 font-medium focus:outline-none outline-none">Update
+                </button>
+              </div>
+            </div>
+            <div class="md:mb-6">
+              <label class="text-gray-600 md:mb-2 md:inline-block">Email</label>
+              <div class="md:flex md:items-center">
+                <input v-if="allowUpdate.email" type="text" v-model="user.email"
+                       class="border border-gray-400 rounded outline-none md:py-1 md:px-2"
+                       name="name">
+                <div v-else>champ.sowmay@gmail.com</div>
+                <button type="button" @click="allowUpdateHandler('email')"
+                        class="md:ml-6 font-medium focus:outline-none outline-none">Update
+                </button>
+              </div>
+            </div>
+            <div class="md:mb-16">
+              <label class="text-gray-600 md:mb-2 md:inline-block">Password</label>
+              <div class="md:flex md:items-center">
+                <input v-if="allowUpdate.password" type="password" v-model="user.password"
+                       class="border border-gray-400 rounded outline-none md:py-1 md:px-2"
+                       name="name">
+                <div v-else>●●●●●●●●</div>
+                <button type="button" @click="allowUpdateHandler('password')"
+                        class="md:ml-6 font-medium focus:outline-none outline-none">Update
+                </button>
+              </div>
+            </div>
+            <div>
+              <label class="md:mb-2 md:inline-block font-medium">Email Preference</label>
+              <div class="md:flex md:items-center">
+                <div>Choose the types of emails you want to receive from HSD</div>
+                <button class="md:ml-6 font-medium focus:outline-none outline-none">Update</button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
-      <div class="md:w-9/12">
-        <form action="">
-          <div class="md:mb-6">
-            <label class="text-gray-600 md:mb-2 md:inline-block">Name</label>
-            <div class="md:flex md:items-center">
-              <input v-if="allowUpdate.name" type="text" v-model="user.name"
-                     class="border border-gray-400 rounded outline-none md:py-1 md:px-2"
-                     name="name">
-              <div v-else>Sowmay</div>
-              <button type="button" @click="allowUpdateHandler('name')" class="md:ml-6 font-medium outline-none focus:outline-none">Update</button>
-            </div>
-          </div>
-          <div class="md:mb-6">
-            <label class="text-gray-600 md:mb-2 md:inline-block">Username</label>
-            <div class="md:flex md:items-center">
-              <input v-if="allowUpdate.username" type="text" v-model="user.username"
-                     class="border border-gray-400 rounded outline-none md:py-1 md:px-2"
-                     name="name">
-              <div v-else>@champ.sowmay</div>
-              <button type="button" @click="allowUpdateHandler('username')"
-                      class="md:ml-6 font-medium focus:outline-none outline-none">Update
-              </button>
-            </div>
-          </div>
-          <div class="md:mb-6">
-            <label class="text-gray-600 md:mb-2 md:inline-block">Email</label>
-            <div class="md:flex md:items-center">
-              <input v-if="allowUpdate.email" type="text" v-model="user.email"
-                     class="border border-gray-400 rounded outline-none md:py-1 md:px-2"
-                     name="name">
-              <div v-else>champ.sowmay@gmail.com</div>
-              <button type="button" @click="allowUpdateHandler('email')"
-                      class="md:ml-6 font-medium focus:outline-none outline-none">Update
-              </button>
-            </div>
-          </div>
-          <div class="md:mb-16">
-            <label class="text-gray-600 md:mb-2 md:inline-block">Password</label>
-            <div class="md:flex md:items-center">
-              <input v-if="allowUpdate.password" type="password" v-model="user.password"
-                     class="border border-gray-400 rounded outline-none md:py-1 md:px-2"
-                     name="name">
-              <div v-else>●●●●●●●●</div>
-              <button type="button" @click="allowUpdateHandler('password')"
-                      class="md:ml-6 font-medium focus:outline-none outline-none">Update
-              </button>
-            </div>
-          </div>
-          <div>
-            <label class="md:mb-2 md:inline-block font-medium">Email Preference</label>
-            <div class="md:flex md:items-center">
-              <div>Choose the types of emails you want to receive from HSD</div>
-              <button class="md:ml-6 font-medium focus:outline-none outline-none">Update</button>
-            </div>
-          </div>
-        </form>
+      <div class="border-t border-gray-400 md:mt-64 md:py-4">
+        <button class="text-red-600 md:font-medium">Delete Account</button>
+        <div>Delete your account and account data</div>
       </div>
     </div>
-    <div class="border-t border-gray-400 md:mt-64 md:py-4">
-      <button class="text-red-600 md:font-medium">Delete Account</button>
-      <div>Delete your account and account data</div>
-    </div>
+    <Footer></Footer>
   </div>
+
 </template>
 
 <script>
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 export default {
   name: "Profile",
+  components: {Footer, Header},
   data() {
     return {
       user: {
@@ -97,3 +112,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+* {
+  font-family: 'Roboto', sans-serif;
+}
+</style>
---
+++ pages/bookmarks.vue
@@ -0,0 +1,224 @@
+<template>
+  <div>
+    <Header>
+      <img id="notification" src="~/assets/images/notification-icon.svg" alt="">
+      <img id="profile" src="~/assets/images/profile.svg" alt="">
+      <button class="primary-btn">write</button>
+    </Header>
+    <div class="md:mx-24 md:my-12">
+      <div class="font-roboto md:flex md:justify-between md:items-center md:mb-12">
+        <div class="md:flex md:items-center">
+          <img class="w-full h-full flex justify-center items-center rounded-full border-2 border-gray-400"
+               src="~/assets/images/profile.svg" alt="">
+          <div class="leading-tight md:ml-3 md:text-sm">
+            <div class="md:font-semibold">Sowmay Jain</div>
+            <div class="text-gray-700">@sowmay_jain</div>
+          </div>
+        </div>
+        <div class="bg-white text-gray-600 md:flex md:rounded md:p-1 md:w-56">
+          <button @click="changeActiveTab" name="Notes" :class="{'activeTab' : activeTab==='Notes'}"
+                  class="outline-none focus:outline-none md:py-2 md:w-6/12 md:rounded">Notes
+          </button>
+          <button @click="changeActiveTab" name="Bookmarks" :class="{'activeTab' : activeTab==='Bookmarks'}"
+                  class="outline-none focus:outline-none md:py-2 md:w-6/12 md:rounded">Bookmarks
+          </button>
+        </div>
+      </div>
+      <div v-if="activeTab==='Bookmarks'" class="auto-rows-fr md:grid md:grid-cols-5 md:gap-6">
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">I was wrong about macOS Big Sur...</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">A month ago, I published my story about macOS Big Sur review. I
+            wrote
+            about how macOS Big Sur has</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">I was wrong about macOS Big Sur...</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">A month ago, I published my story about macOS Big Sur review. I
+            wrote
+            about how macOS Big Sur has</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">Jeff Bezos Swallowed Our Economy Whole</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Amid social, economic, and political chaos, regulators and
+            antitrust
+            lawyers on both sides of the Atlantic are circling Big Tech, with the aim of bringing Facebook, Google, and
+            Apple to heel. </p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">Lou Dobbs’s Show Is Canceled by Fox</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Mr. Dobbs, a loyal supporter of former President Donald J.
+            Trump, was
+            named in a $2.7 billion lawsuit filed on Thursday against the Fox Corporation and two other Fox anchors.</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">The 55 Richest Women In America</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Walton (net worth: $62.3 billion), an heir to the Walmart
+            fortune, saw
+            her net worth climb nearly $11 billion since the 2019 Forbes 400 due to an uptick in Walmart stock after the
+            company pivoted to online shopping for groceries and home goods during the pandemic.</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">I was wrong about macOS Big Sur...</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">A month ago, I published my story about macOS Big Sur review. I
+            wrote
+            about how macOS Big Sur has</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">I was wrong about macOS Big Sur...</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">A month ago, I published my story about macOS Big Sur review. I
+            wrote
+            about how macOS Big Sur has</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">Jeff Bezos Swallowed Our Economy Whole</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Amid social, economic, and political chaos, regulators and
+            antitrust
+            lawyers on both sides of the Atlantic are circling Big Tech, with the aim of bringing Facebook, Google, and
+            Apple to heel. </p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">Lou Dobbs’s Show Is Canceled by Fox</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Mr. Dobbs, a loyal supporter of former President Donald J.
+            Trump, was
+            named in a $2.7 billion lawsuit filed on Thursday against the Fox Corporation and two other Fox anchors.</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">The 55 Richest Women In America</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Walton (net worth: $62.3 billion), an heir to the Walmart
+            fortune, saw
+            her net worth climb nearly $11 billion since the 2019 Forbes 400 due to an uptick in Walmart stock after the
+            company pivoted to online shopping for groceries and home goods during the pandemic.</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">I was wrong about macOS Big Sur...</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">A month ago, I published my story about macOS Big Sur review. I
+            wrote
+            about how macOS Big Sur has</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">I was wrong about macOS Big Sur...</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">A month ago, I published my story about macOS Big Sur review. I
+            wrote
+            about how macOS Big Sur has</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">Jeff Bezos Swallowed Our Economy Whole</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Amid social, economic, and political chaos, regulators and
+            antitrust
+            lawyers on both sides of the Atlantic are circling Big Tech, with the aim of bringing Facebook, Google, and
+            Apple to heel. </p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">Lou Dobbs’s Show Is Canceled by Fox</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Mr. Dobbs, a loyal supporter of former President Donald J.
+            Trump, was
+            named in a $2.7 billion lawsuit filed on Thursday against the Fox Corporation and two other Fox anchors.</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">The 55 Richest Women In America</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Walton (net worth: $62.3 billion), an heir to the Walmart
+            fortune, saw
+            her net worth climb nearly $11 billion since the 2019 Forbes 400 due to an uptick in Walmart stock after the
+            company pivoted to online shopping for groceries and home goods during the pandemic.</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+      </div>
+      <div v-else class="md:grid auto-rows-fr md:grid-cols-5 md:gap-6">
+        <div
+          class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:items-center md:justify-center md:flex-col md:p-5">
+          <div>
+            <img class="" src="~/assets/images/note.svg" alt="">
+            <div class="text-lg text-gray-600 font-roboto">NEW</div>
+          </div>
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">I was wrong about macOS Big Sur...</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">A month ago, I published my story about macOS Big Sur review. I
+            wrote
+            about how macOS Big Sur has</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">I was wrong about macOS Big Sur...</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">A month ago, I published my story about macOS Big Sur review. I
+            wrote
+            about how macOS Big Sur has</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">Jeff Bezos Swallowed Our Economy Whole</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Amid social, economic, and political chaos, regulators and
+            antitrust
+            lawyers on both sides of the Atlantic are circling Big Tech, with the aim of bringing Facebook, Google, and
+            Apple to heel. </p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">Lou Dobbs’s Show Is Canceled by Fox</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Mr. Dobbs, a loyal supporter of former President Donald J.
+            Trump, was
+            named in a $2.7 billion lawsuit filed on Thursday against the Fox Corporation and two other Fox anchors.</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">The 55 Richest Women In America</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">Walton (net worth: $62.3 billion), an heir to the Walmart
+            fortune, saw
+            her net worth climb nearly $11 billion since the 2019 Forbes 400 due to an uptick in Walmart stock after the
+            company pivoted to online shopping for groceries and home goods during the pandemic.</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+        <div class="font-tinos bg-white border border-gray-200 rounded-md shadow-sm md:flex md:flex-col md:p-5">
+          <h3 class="md:font-bold md:text-lg md:mb-1">I was wrong about macOS Big Sur...</h3>
+          <p class="md:text-sm md:font-normal md:flex-1">A month ago, I published my story about macOS Big Sur review. I
+            wrote
+            about how macOS Big Sur has</p>
+          <img class="md:ml-auto md:self-end" src="~/assets/images/bookmark.svg" alt="">
+        </div>
+      </div>
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default {
+  name: "bookmarks",
+  data() {
+    return {
+      activeTab: 'Bookmarks'
+    }
+  },
+  components: {Footer, Header},
+  methods: {
+    changeActiveTab(e) {
+      const {name} = e.target;
+      console.log(name)
+      this.activeTab = name
+    }
+  }
+}
+</script>
+
+<style scoped>
+.activeTab {
+  @apply bg-gray-300 text-black;
+}
+</style>
